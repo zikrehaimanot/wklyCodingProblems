7fc04ef34b8ab83b6e791089ed21e70ed1a2de3c
zikrehaimanot <zikrehaimanot@gmail.com>
2020-04-09 12:23:57 -0400

    edit

---
+++ third.js
@@ -0,0 +1,16 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+
+
+
+
+
+var findNumbers = function(nums) {
+    let count = 0;
+    for (i = 0; i < nums.length; i++){
+        if(nums[i].toString().length % 2 === 0){
+            count += 1;
+        }
+    }
+    return count;
+};
